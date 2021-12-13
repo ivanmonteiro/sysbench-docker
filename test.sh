a84e34a802c2b48edfa5370f113a9fd5666a0865
Ivan Monteiro Cantalice <ivanmonteiroc@gmail.com>
2021-12-13 19:40:14 -0300

    Change thread count

--- test.sh
+++ test.sh
@@ -8,8 +8,8 @@ sysbench --mysql-host=mariadb  --mysql-db=stresstest --mysql-user=testuser  \
 # run tests
 sysbench --mysql-host=mariadb  --mysql-db=stresstest --mysql-user=testuser \
          --max-requests=0  --db-driver=mysql --mysql-password=testuser \
-         --time=15  --report-interval=5 \
-         --mysql-port=3306 --threads=128 /usr/share/sysbench/$1 run
+         --time=60  --report-interval=5 \
+         --mysql-port=3306 --threads=2 /usr/share/sysbench/$1 run
 # cleanup
 sysbench --mysql-host=mariadb  --mysql-db=stresstest --mysql-user=testuser \
          --db-driver=mysql --mysql-password=testuser \
